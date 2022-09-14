098fd13493660ed95eb1105171811883cbae5ba1
emresahin <emre10sahin10@gmail.com>
2022-09-15 02:23:04 +0300

    Add version catalog and change gradle files to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.junit)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.kotlinParcelize)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktLint)
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.emresahin.metmuseum"
+        minSdk = 23
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.get()
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.production)
+    kapt(libs.hiltCompiler)
+    ksp(libs.roomCompiler)
+
+    debugImplementation(libs.bundles.debug)
+
+    testImplementation(libs.bundles.test)
+    testRuntimeOnly(libs.testJunitEngine)
+    androidTestImplementation(libs.bundles.testAndroid)
+    kaptAndroidTest(libs.testAndroidHiltCompiler)
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,5 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Metropolitan Museum"
-include ':app'
+include (":app")
+enableFeaturePreview("VERSION_CATALOGS")
